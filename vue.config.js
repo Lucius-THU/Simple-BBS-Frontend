ac0f48042458dc1588006aace0da112e9117ca45
Lucius <souzou@foxmail.com>
2020-08-20 20:21:44 +0800

    modify format

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://simplebbs.iterator-traits.com',
-        ws: true,
-        changeOrigin: true
-      }
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://simplebbs.iterator-traits.com',
+                ws: true,
+                changeOrigin: true
+            }
+        }
     }
-  }
 }
